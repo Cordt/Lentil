170cf1d382e2802ec9898a0dccac06b6dbc3fbbf
Cordt <1705816+Cordt@users.noreply.github.com>
2023-02-06 08:51:35 +0200

    Added test for loading bundle from Keychain

--- LentilTests/ConversationsTests/ConversationsTests.swift
+++ LentilTests/ConversationsTests/ConversationsTests.swift
@@ -1,20 +1,43 @@
 // LentilTests
 
 import ComposableArchitecture
+import IdentifiedCollections
 import XCTest
 @testable import Lentil
 
 @MainActor
 final class ConversationsTests: XCTestCase {
   
-  func testBundleIsLoadedFromKeychain() {
+  func testBundleIsLoadedFromKeychain() async throws {
     let store = TestStore(
       initialState: Conversations.State(),
       reducer: Conversations()
-    )
+    ) {
+      $0.cache.profileByAddress = { _ in MockData.mockProfiles[1] }
+      $0.xmtpConnector.address = { "0xabcdef" }
+      $0.xmtpConnector.loadConversations = { MockData.conversations }
+      $0.xmtpConnector.tryLoadCLient = { true }
+    }
     
-    store.dependencies.xmtpConnector.tryLoadCLient = { true }
+    let conversationRows = MockData.conversations.map { conversation in
+      ConversationRow.State(
+        conversation: conversation,
+        userAddress: "0xabcdef",
+        lastMessage: .init(
+          message: MockData.messages[5],
+          from: .peer
+        ),
+        profile: MockData.mockProfiles[1]
+      )
+    }
     
-    store.send(.didAppear)
+    await store.send(.didAppear)
+    await store.receive(.loadConversations) {
+      $0.connectionStatus = .signedIn
+    }
+    await store.receive(.loadConversations)
+    await store.receive(.conversationsResult(.success(conversationRows))) {
+      $0.conversations = IdentifiedArrayOf(uniqueElements: conversationRows)
+    }
   }
 }
